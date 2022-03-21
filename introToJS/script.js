a26b650c053fee5d6e9902016db87bfd7cb3204f
Hector Gonzalez <hectorg@MacBook-Pro-2.local>
2022-03-21 11:19:14 -0400

    adding design and js

---
+++ introToJS/script.js
@@ -0,0 +1,128 @@
+// Lesson 16 Intro to Js
+
+// var x = 4;
+// var myVariable = 'table';
+// var tree = 'ceder';
+// let myStringNumber = '1242345';
+// let myLargeNumber = 1324098258;
+// let asdf = null;
+// let theTruth = true;
+// let something = [2,24,6,6,43,3];
+// // let myFunction = function(){};
+
+// // console.log(typeof myFunction);
+
+
+// let num = 7 + 7 + '7';
+
+// let num2 = "3" + 3 + 3 + 3 + 3;
+
+// console.log(typeof num2);
+
+// console.log(typeof num2);
+
+// console.log(parseInt(num2));
+
+// console.log(typeof parseInt(num2));
+
+// Null and undefined
+
+// let x = null;
+// let y = undefined;
+// let z;
+
+// console.log(z);
+
+// Flow Control
+
+// let myVariable = false;
+
+// if (myVariable) {
+//     console.log('myVariable is greater than 2');
+// } else {
+//     console.log('myVariable is not greater than 2');
+// }
+
+// let num = 3;
+// let num2 = 4;
+
+// num -= 3;
+// num2 = num2 - 4;
+
+// console.log(num);
+// console.log(num2);
+
+
+// if (num) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+// AND OR
+
+// let num = 23;
+// let num2 = 34;
+
+// if (num > 30 || num2 > 30) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+// }
+
+
+// LOOPS
+
+// let variable = 20;
+
+// while (variable > 0) {
+//     console.log(variable);
+//     variable++;
+// }
+
+// do {
+//     console.log('run');
+// } while (variable);
+
+// Initialization 
+// Condition
+// Final Statement
+
+// let str = '';
+
+// for (let i=0; i < 10; i++) {
+//     str += i;
+// }
+
+// let str = '';
+// let i = 0;
+
+// while (i < 10) {
+//     str += i;
+//     i ++;
+// }
+// console.log(str);
+
+// let str = '';
+// let i = 0;
+
+// do {
+//     str += i;
+//     i ++;
+// } while (i < 10) 
+
+// console.log(str);
+
+// for (let i=0; i<10; i++) {
+//     console.log(i);
+//     console.log('before break');
+//     break;
+//     console.log('after break');
+// }
+
+// for (let i=0; i<10; i++) {
+//     console.log(i);
+//     console.log('before break');
+//     continue;
+//     console.log('after break');
+// }

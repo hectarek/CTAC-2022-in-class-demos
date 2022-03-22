da53e392cace0c61cbc4de0e9ce1a0dd03e18893
Hector Gonzalez <hectorg@macbook-pro-2.attlocal.net>
2022-03-22 11:56:15 -0400

    adding js data structures

---
+++ jsDataStructures/script.js
@@ -0,0 +1,73 @@
+// Day 17 Data Structures
+
+// function multiply(x,y) {
+//     return x * y;
+// }
+
+// console.log(multiply(4,5));
+
+// let y = 35;
+// function add(x,y) {
+//     return x + y;
+// }
+
+// console.log(add(3,3));
+
+
+// function saySomthing() {
+//     console.log('Hello Console!');
+// }
+
+// console.log(saySomthing());
+
+// ARRAYS
+
+// let fruit = 'apple';
+// const fruits = ['melon', 'mango', 'coconut'];
+
+// console.log(fruit);
+// console.log(fruits);
+
+// fruits[1] = 'mangosteen';
+// fruits[2] = 'starfruit';
+
+// console.log(fruits);
+
+
+// const fruits = ['melon', 'mango', 'coconut'];
+
+// console.log(fruits.length);
+
+// let multiFruits = ['apples', 'bananas','orange', 'kiwi'];
+// multiFruits.push('grapes');
+// multiFruits.pop();
+// multiFruits.unshift('lemons');
+// multiFruits.shift();
+
+// console.log(multiFruits.sort());
+
+// let nums = [1,5,3,34,22,6,3,2,16];
+
+// console.log(nums.sort());
+
+// let trees = ['ceder', 'fir', 'evergreen', 'maple'];
+
+// console.log(trees.slice(1,3));
+
+// OBJECTS
+
+// let user = {
+//     name: 'Kimmy Schmitt',
+//     email: ['kimmy@mail.com', 'kimmyS@gmail.com'],
+//     dependent: {
+//         name: 'John',
+//         age: 45,
+//         email: 'john@mail.com'
+//     }
+// }
+
+// user.name = 'Antonio B';
+
+// console.log(Object);
+
+
